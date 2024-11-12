a5f6539e008abe5d79569ed38606cbbec74f8724
Eddumonte <eduardomonte_93@hotmail.com>
2024-11-11 22:41:15 -0300

    inicio del proyecto

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar () {
+    return (
+        <nav className="nav">
+            <ul className="Ul">
+                <li className="li"><a href="" className="a"></a></li>
+                <li className="li"><a href="" className="a"></a></li>
+                <li className="li"><a href="" className="a"></a></li>
+                <li className="li"><a href="" className="a"></a></li>
+            </ul>
+        </nav>
+    )
+}
